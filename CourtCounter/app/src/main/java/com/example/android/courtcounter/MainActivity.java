c586818e37868db46d6ca80ad67ca63fc14f2826
levontopalakian <levontopalakian@gmail.com>
2018-06-02 00:25:19 +0300

    adding second team

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -13,27 +13,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    int scoreA = 0;
+    int scoreB = 0;
+    public void freeA (View view){
+        scoreA = scoreA+1;
+        displayForTeamA(scoreA);
+    }
 
-    public void free (View view){
-        displayForTeamA(1);
+    public void freeB (View view){
+        scoreB = scoreB+1;
+        displayForTeamB(scoreB);
     }
 
 
-    public void threepoints (View view){
-        displayForTeamA(3);
+    public void threepointsA (View view){
+        scoreA = scoreA+3;
+        displayForTeamA(scoreA);
+    }
+    public void threepointsB (View view){
+        scoreB = scoreB+3;
+        displayForTeamB(scoreB);
     }
 
 
-    public void twopoints (View view){
-        displayForTeamA(2);
+    public void twopointsA (View view){
+        scoreA = scoreA+2;
+        displayForTeamA(scoreA);
     }
-
+    public void twopointsB (View view){
+        scoreB = scoreB+2;
+        displayForTeamB(scoreB);
+    }
 
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
 }
