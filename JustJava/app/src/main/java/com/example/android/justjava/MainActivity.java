875e098171e8b017ef4feadd882641211b865155
levontopalakian <levontopalakian@gmail.com>
2018-06-17 17:25:23 +0300

    corecting negative values

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 0;
+    int quantity = 1;
 
-    private String createOrderSummery(int price, boolean haswhippedCream,boolean haschocolate) {
-        String priceMessage = "Name : Levon Topalakian";
+    private String createOrderSummery(int price, boolean haswhippedCream,boolean haschocolate,String customerName) {
+        String priceMessage = "Name : "+customerName;
         priceMessage += "\nAdd Whipped Cream? " + haswhippedCream;
         priceMessage += "\nAdd Chocolate? " + haschocolate;
         priceMessage += "\nQuantity : " + quantity;
@@ -31,26 +33,46 @@ public class MainActivity extends AppCompatActivity {
        //Log.v ("MainActivity","Has whipped cream: "+haswhippedCream);
         CheckBox whippedCream = (CheckBox) findViewById(R.id.whippedCream);
         boolean haswhippedCream = whippedCream.isChecked();
-        int price = calculatePrice();
         CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate);
         boolean haschocolate = chocolate.isChecked();
-        createOrderSummery(price,haswhippedCream,haschocolate);
+        int price = calculatePrice(haswhippedCream,haschocolate);
+        EditText customername = (EditText) findViewById(R.id.nameinput);
+        String customerName = customername.getText().toString();
+        createOrderSummery(price,haswhippedCream,haschocolate,customerName);
     }
 
     public void increment(View view) {
+        if (quantity == 100){
+            Toast.makeText(this,"This is the maximum quantity",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity++;
+
         display(quantity);
     }
 
     public void decrament(View view) {
+        if (quantity == 1){
+             Toast.makeText(this,"This is the minimum quantity",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity--;
+
         display(quantity);
     }
 
 
 
-    private int calculatePrice() {
-        int price = quantity * 8;
+    private int calculatePrice(boolean haswhippedCream,boolean haschocolate) {
+        int addional = 0;
+        int coffe = 5;
+        if (haswhippedCream){
+            addional = addional+1;
+        }
+        if (haschocolate) {
+            addional = addional + 2;
+        }
+        int price = (coffe+addional)*quantity;
         return price;
 
     }
